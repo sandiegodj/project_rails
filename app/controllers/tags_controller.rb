bab7716929d0bf222f3f6a7ff1241946e210f3ff
sandiegodj <davidjanczyn@gmail.com>
2014-07-12 23:17:06 -0700

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,20 @@
+class TagsController < ApplicationController
+	def show
+		@tag = Tag.find(params[:id])
+	end
+	def index
+		@tags = Tag.all
+	end
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.delete
+
+		flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+		redirect_to tag_path
+
+	end
+
+
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-#This used to be rails
+
 Blogger::Application.routes.draw do
   root to:'articles#index'
   resources :articles do
     resources :comments
   end
+  resources :tags
 end
